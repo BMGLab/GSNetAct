d230680293bc6008f5c462929943ceb1df361209
sadigngr <05210000868@ogrenci.ege.edu.tr>
2025-03-21 12:58:58 +0300

    fix output

--- test/adatatest.py
+++ test/adatatest.py
@@ -6,9 +6,8 @@ adata_ = "./test_data/pbmc3k.h5ad"
 jsonFile = "./test_data/big_genesets_relations.json"
 
 _adata = createObject(adata_, jsonFile, normalized=True)
-
 df = pd.DataFrame(_adata.X)
-print(df.shape)
+df.columns = _adata.var
 
-df.to_csv("output.csv", index=False, header=False)
+df.to_csv("output.csv",sep="\t")
 
