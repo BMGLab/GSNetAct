3d13142af8bd3f1419a11e31698d5973b27ec90f
sadigngr <05210000868@ogrenci.ege.edu.tr>
2025-03-21 12:31:08 +0300

    Add output file

--- test/adatatest.py
+++ test/adatatest.py
@@ -1,8 +1,14 @@
 from Scoring._annData import createObject
+import pandas as pd
+import numpy as np
 
 adata_ = "./test_data/pbmc3k.h5ad"
 jsonFile = "./test_data/big_genesets_relations.json"
 
-_adata = createObject(adata_,jsonFile,normalized=True)
+_adata = createObject(adata_, jsonFile, normalized=True)
 
-print(_adata)
+df = pd.DataFrame(_adata.X)
+print(df.shape)
+
+df.to_csv("output.csv", index=False, header=False)
+
